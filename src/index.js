6c8b2dd16d5d25789e7b61545a18b41a5375273f
kiend <quangdang271105@gmail.com>
2025-03-14 11:15:49 +0100

    rework the index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const express = require("express")
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 
+const randomInt = (max) => Math.floor(Math.random() * max);
+
+// Middleware to calculate an arithmetic value
+app.use((req, res, next) => {
+    req.arithmetic_calculation = randomInt(111) * randomInt(200); 
+    next();
+});
+
+// Route handler
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+    console.log(`Calculated Value: ${req.arithmetic_calculation}`); // Log the calculated value
+    res.send(`Hello there! The calculated value is ${req.arithmetic_calculation}`);
+});
 
+// Start server
 app.listen(port, () => {
-    console.log('Example app installing ')
-})
+    console.log(`Example app running at http://localhost:${port}`);
+});

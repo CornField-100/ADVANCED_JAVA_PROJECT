ced6fede88a442309b970f1b2fe2cc814e12aae9
kiend <quangdang271105@gmail.com>
2025-03-17 14:36:38 +0100

    added hashing

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const router = express.Router()
+const { hashPassword } = require("../middleware/passencrypt")
 
 router.get('/', (req, res) => {
     res.send('Welcome to my API ! e-commerce backed')
 })
 
-router.post("/", (req, res) => {
+router.post("/", hashPassword, (req, res) => {
     // get the data from the request
-    const { firstName, email, password } = req.body
+    const { firstName, email } = req.body
+    const hashedPassword = req.hashedPassword
     res.json({
         firstName,
         email,
-        password,
+        hashedPassword,
         _id: "randomId1234"
     })
     console.log(firstName, email, password, )

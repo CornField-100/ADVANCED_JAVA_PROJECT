af9c1d72cbc4cf349f7e37e31f2593d43c0f3d7c
kiend <quangdang271105@gmail.com>
2025-03-17 16:18:03 +0100

    designed a db connection

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const userRoutes = require("./src/routes/users")
 
 const randomInt = (max) => Math.floor(Math.random() * max);
 
-// Middleware to calculate an arithmetic value
-app.use((req, res, next) => {
-    req.arithmetic_calculation = randomInt(111) * randomInt(200); 
-    next();
-});
+//Connect to DB
+const connectDB = require("./src/utils/db")
+
 //New middleware 
 app.use(express.json())
 
-// Route handler
-app.get("/", (req, res) => {
-    console.log(`Calculated Value: ${req.arithmetic_calculation}`); // Log the calculated value
-    res.send(`Hello there! The calculated value is ${req.arithmetic_calculation}`);
-});
+// cors middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    )
+    next()
+})
+
+connectDB()
 
 // Start server
 app.listen(port, () => {

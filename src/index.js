5f8c2deb45ae6cc591d6e55b10435cf18097e0aa
kiend <quangdang271105@gmail.com>
2025-06-02 16:04:19 +0200

    fix: changed the CORS middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const port = 3001;
 const userRoutes = require("./routes/users")
 const productRoutes = require("./routes/products")
 const invoiceRoutes = require("./routes/invoices");
-const path = require("path")
+const path = require("path");
+const cors = require("cors");
 
 const randomInt = (max) => Math.floor(Math.random() * max);
 
@@ -18,14 +19,11 @@ app.use(express.json())
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 // cors middleware
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    )
-    next()
-})
+app.use(cors({
+  origin: "https://frontendjava.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 connectDB()
 
